7898170cf89b739772facfdc76b589286809b9a5
Robin Hos <robin.hos@gmail.com>
2019-12-11 19:20:42 +0100

    [ADD] endpoints for missing entities

--- docs-src/_plugins/generate-api.rb
+++ docs-src/_plugins/generate-api.rb
@@ -24,22 +24,102 @@ module Jekyll
     priority :low
 
     def generate(site)
+      baseDir = "api/v1"
 
-      endpoint site, "api/v1", "events"
+      @site = site
 
-      endpoint site, "api/v1", "teams"
+      file = File.read(File.join(site.source, "_data", "entities.json"))
 
-      endpoint site, "api/v1", "persons", []
+      @entities = JSON.parse(file)
+
+      @schemas = {
+        "events" => {
+          "participants" => {
+            "mapper" => lambda { | participant | 
+              id = participant["id"]
+              type = participant["type"]
+              number = participant["number"]
+              result = participant["result"]
+              formattedResult = participant["formattedResult"]
+
+              item = {
+                "competitor" => {
+                  "id" => id,
+                  "type" => type,
+                  "name" => @entities["#{type}s"][id]["name"]
+                },
+                "result" => result,
+                "formattedResult" => formattedResult
+              }
+
+              item["number"] = number unless number.nil?
+
+              item
+            }
+          }
+        },
+        "teams" => {
+          "members" => {
+            "mapper" => lambda { | member | { "name" => @entities["persons"][member["id"]]["name"] }.merge(member) }
+          }
+       }
+      }
+
+      ["events", "teams", "persons"].each { | name |
+        generateEntities baseDir, name, @entities[name]
+      }
 
     end
 
-    def endpoint(site, dir, name, data = nil)
-      data = site.data[name] unless data
-      page = DataPage.new(site, site.source, dir, "#{name}.json")
-      page.data["layout"] = nil
-      page.content = JSON.generate(data)
+    def generateEntities(dir, name, data, meta = nil)
+      if (data.nil?)
+         return nil
+      end
 
-      site.pages << page
+      items = nil.to_a
+
+      entityDir = File.join(dir, name)
+
+      data.entries.each { | id, attributes | 
+        item = generateEntity entityDir, name, id, attributes, meta
+        items.push(item)
+      }
+
+      endpoint dir, name, items
+    end
+
+    def generateEntity(dir, name, id, attributes, meta = nil)
+      schema = @schemas[name]
+
+      item = nil
+
+      if schema.nil?
+        item = { "id" => id }.merge(attributes)
+      else
+        item = { "id" => id }.merge(attributes).select { | key, value | !schema.keys.include? key }
+
+        relationDir = File.join(dir, id)
+
+        schema.entries.each { | relationName, relationMeta |
+          generateEntities relationDir, relationName, attributes[relationName], relationMeta
+        }
+      end
+
+      if !meta.nil?
+        item = meta["mapper"].(item)
+      end
+      
+      endpoint(dir, id, item) unless schema.nil?
+
+      item
+    end
+
+    def endpoint(dir, name, data)
+      page = DataPage.new(@site, @site.source, dir, "#{name}.json")
+      page.data["layout"] = nil
+      page.content = JSON.pretty_generate(data)
+
+      @site.pages << page
     end
 
   end
